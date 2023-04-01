b65d25172428047dfc64d9f5d5b6725bff84dd0c
lin274606 <lin274606@gmail.com>
2023-04-01 19:39:30 +0800

    update

--- MyCal/MyCalculater.cs
+++ MyCal/MyCalculater.cs
@@ -16,36 +16,60 @@ namespace MyCal
         {
             InitializeComponent();           
         }
-        
+        private double GetNum1()
+        {
+            double num1;
+            if (!double.TryParse(txtNum1.Text, out num1))
+            {
+                MessageBox.Show("請輸入有效的數字！");
+            }
+            return num1;
+        }
+
+        private double GetNum2()
+        {
+            double num2;
+            if (!double.TryParse(txtNum2.Text, out num2))
+            {
+                MessageBox.Show("請輸入有效的數字！");
+            }
+            return num2;
+        }
+
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
-             int num1 = int.Parse(txtNum1.Text);
-             int num2 = int.Parse(txtNum2.Text);
-             int ans=num1 + num2;
+            double num1 = GetNum1();
+            double num2 = GetNum2();
+            double ans = num1 + num2;
              txtAns.Text = Convert.ToString(ans);
         }
 
         private void btnSub_Click(object sender, EventArgs e)
         {
-            int num1 = int.Parse(txtNum1.Text);
-            int num2 = int.Parse(txtNum2.Text);
-            int ans = num1 - num2;
+            double num1 = GetNum1();
+            double num2 = GetNum2();
+            double ans = num1 - num2;
             txtAns.Text = Convert.ToString(ans);
         }
 
         private void btnMul_Click(object sender, EventArgs e)
         {
-            double num1 = int.Parse(txtNum1.Text);
-            double num2 = int.Parse(txtNum2.Text);
+            double num1 = GetNum1();
+            double num2 = GetNum2();
             double ans = num1 * num2;
             txtAns.Text = Convert.ToString(ans);
         }
 
         private void btnDiv_Click(object sender, EventArgs e)
         {
-            double num1 = int.Parse(txtNum1.Text);
-            double num2 = int.Parse(txtNum2.Text);
+            double num1 = GetNum1();
+            double num2 = GetNum2();
+            if (num2 == 0)
+            {
+                MessageBox.Show("除數不能為零！");
+                return;
+            }
             double ans = num1 / num2;
             txtAns.Text = Convert.ToString(ans);
         }
